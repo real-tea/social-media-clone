c14cb33923045292c0badaa325b8f1a875adb3f9
Akash <akash.0711a@gmail.com>
2021-12-08 12:50:22 +0530

    unfollowing a user

--- routes/users.js
+++ routes/users.js
@@ -46,12 +46,12 @@ Router.delete("/:id",async (req,res)=>{
 });  
 
 //get a user
-//understand again
+
 Router.get("/:id",async (req,res) => {
     try{
         const user = await User.findById(req.params.id)
-        const { password , updatedAt, ...other } = user._doc;
-        res.status(200).json(other);
+        const { password , updatedAt, ...other } = user._doc; //_doc is the instance of the model
+        res.status(200).json(other); //password and updated at won't be send and all parameters in "other" field will be send
     }catch(err){
         res.status(500).json(err);
     }
@@ -59,15 +59,15 @@ Router.get("/:id",async (req,res) => {
 
 //follow a user 
 
-Router.put("/:id?follow",async(req,res)=>{
+Router.put("/:id/follow",async(req,res)=>{
     if(req.body.userId !== req.params.id){
         try{
             const user = await User.findById(req.params.id);
             const currentUser = await User.findById(req.body.userId);
-            if(!user.followers.includes(req.body.userId)){
+            if(!user.followers.includes(req.body.userId)){//currentUser 
                 await user.updateOne({ $push : {followers : req.body.userId} });
-                await currentUser.updateOne({ $push : {followings : req.params.body.userId} });
-                res.status(200).json({message : "user has been followed"});
+                await currentUser.updateOne({ $push : {followings : req.params.id} });
+                res.status(200).json("user has been followed");
             }else{
                 res.status(403).json("you already follow this user");
             }
@@ -75,7 +75,35 @@ Router.put("/:id?follow",async(req,res)=>{
         }catch(err){
             res.status(500).json(err);
         }
-    }else{res.status(403).json("user can't be followed")}
+    }else{
+        res.status(403).json("user can't be followed")
+    }
+})
+
+//unfollow a user : 
+
+Router.put("/:id/unfollow", async (req,res) =>{
+    if(req.body.userId !==req.params.id)
+    { 
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({ $pull : {followers : req.body.userId} });
+                await currentUser.updateOne({ $pull : { following : req.params.id } });
+                res.status(200).json("user has been unfollowed");
+            }else{
+                res.status(403).json("you already unfollowed the user");
+            }
+
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }
+    else{
+        res.status(403).json("you unfollowed the user")
+    }
+
 })
 
 module.exports = Router;
